81dd72b3d8c1e87ff649380b2321aa62a405553f
Tomas Gonzalez Humphreys <tomasgonzalezhu.phreys12@gmail.com>
2025-11-10 11:32:32 -0300

    Se agrego Fisher-Snedecor (F)

--- prob_distribution.py
+++ prob_distribution.py
@@ -16,7 +16,8 @@ modelos = [
     "Normal",
     "Log Normal",
     "Gamma - Poisson",
-    "Gamma - Empírica"
+    "Gamma - Empírica",
+    "Fisher-Snedecor (F)"
 ]
 def render():
     # Título de la app
@@ -24,7 +25,7 @@ def render():
     modelo_seleccionado = st.selectbox("Selecciona un modelo de distribución:", modelos)
 
     # Función para calcular la probabilidad
-    def calcular_probabilidad(modelo, params, x, lado):
+    def calcular_probabilidad(modelo, params, x, lado, prob):
         try:
             if modelo == "Proceso de Bernoulli - Modelo Binomial":
                 n, p = params['n'], params['p']
@@ -70,9 +71,16 @@ def render():
                 k, theta = params['k'], params['theta']
                 dist = stats.gamma(a=k, scale=theta)
                 cdf = dist.cdf(x)
+            elif modelo == "Fisher-Snedecor (F)":
+                df1, df2 = params['df1'], params['df2']
+                dist = stats.f(dfn=df1, dfd=df2)
             
-            if lado == "Izquierda":
+            if lado == "Izquierda" and modelo == "Fisher-Snedecor (F)":
+                return dist.ppf(prob)
+            elif lado == "Izquierda":
                 return cdf
+            elif lado == "Derecha" and modelo == "Fisher-Snedecor (F)":
+                return dist.ppf(prob)
             elif lado == "Derecha":
                 return 1 - cdf
         except Exception as e:
@@ -108,14 +116,19 @@ def render():
     elif modelo_seleccionado == "Gamma - Empírica":
         params['k'] = st.number_input("Shape (k)", min_value=0.100, value=1.0, format="%0.6f")
         params['theta'] = st.number_input("Scale (θ)", min_value=0.100, value=1.0, format="%0.6f")
+    elif modelo_seleccionado == "Fisher-Snedecor (F)":
+        params['df1'] = st.number_input("Grados de libertad numerador (df1)", min_value=1, value=5)
+        params['df2'] = st.number_input("Grados de libertad denominador (df2)", min_value=1, value=10)
+        prob = st.number_input("Probabilidad", min_value=0.000, max_value=1.0, value=0.5, format="%0.6f")
 
     # Input para x y selección de lado
     x = st.number_input("Valor de x", value=0.0, format="%0.6f")
+
     lado = st.selectbox("Selecciona el lado de la probabilidad:", ["Izquierda", "Derecha"])
 
     # Botón para calcular
     if st.button("Calcular Probabilidad"):
-        prob = calcular_probabilidad(modelo_seleccionado, params, x, lado)
+        prob = calcular_probabilidad(modelo_seleccionado, params, x, lado, prob)
         if isinstance(prob, str):
             st.error(prob)
         else:
